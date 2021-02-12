229df343878b9874b12ce831296027570e77ac45
Scott Kuykendall <scott.kuykendall@doesis.pas>
2021-02-12 08:54:14 -0500

    use local nuget for db restores

--- DatabaseTemplate/Scripts/EdFiMinimalTemplate.ps1
+++ DatabaseTemplate/Scripts/EdFiMinimalTemplate.ps1
@@ -3,18 +3,18 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-  $params = @{
-      packageName    = "EdFi.Suite3.Ods.Minimal.Template"
-      packageVersion = "5.1.0"
-      packageSource  = "https://www.myget.org/F/ed-fi/"
-  }
- # We cannot reach myget from the database server.
- #The templates are in the repo, so I parsed the path to them and submitted it
-
  # $params = @{
      # packageName    = "EdFi.Suite3.Ods.Minimal.Template"
      # packageVersion = "5.1.0"
-     # packageSource  = "https://nuget.doe.k12.de.us/AppDevNugetServer/"
+     # packageSource  = "https://www.myget.org/F/ed-fi/"
  # }
+ # We cannot reach myget from the database server.
+ #The templates are in the repo, so I parsed the path to them and submitted it
+
+ $params = @{
+     packageName    = "EdFi.Suite3.Ods.Minimal.Template"
+     packageVersion = "5.1.0"
+     packageSource  = "https://nuget.doe.k12.de.us/AppDevNugetServer/nuget"
+ }
 
 & "$PSScriptRoot\..\Modules\get-populated-from-nuget.ps1" @params
