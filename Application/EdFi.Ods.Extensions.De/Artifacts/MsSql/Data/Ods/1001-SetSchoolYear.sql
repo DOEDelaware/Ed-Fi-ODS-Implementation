a3da3aafa9429a04fa2a6373145f03f7907f2406
suman budde <suman.budde@gmail.com>
2021-03-24 16:15:14 -0400

    LearningModality files

--- Application/EdFi.Ods.Extensions.De/Artifacts/MsSql/Data/Ods/1001-SetSchoolYear.sql
+++
@@ -1,4 +0,0 @@
-update edfi.schoolYearType set CurrentSchoolYear=0
-go
-update edfi.schoolYearType set CurrentSchoolYear=1 where schoolYear=2022
-go
--- Application/EdFi.Ods.Extensions.De/Artifacts/MsSql/Data/Ods/1005-DescriptorFunction.sql
+++
@@ -1,9 +0,0 @@
-
- create function [dbo].[descriptor]( @ns varchar(255), @codevalue varchar(50)) returns int as
- begin
- declare @result int
- select @result=descriptorid from edfi.descriptor where codevalue = @codevalue and namespace like  @ns 
-return @result
- end
- GO
- 
