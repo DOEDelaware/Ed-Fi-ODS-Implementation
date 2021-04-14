2282e9996c8cc5395079ccabe70a05ebdb6528b9
Scott Kuykendall <scott.kuykendall@doesis.pas>
2021-04-14 12:15:23 -0400

    fix for sandbox and testing around StudentEdOrgResp

--- Scripts/Delaware Scripts/Database_Data_EdFiSecurity/old/10031-fix for sandbox.sql
+++ Scripts/Delaware Scripts/Database_Data_EdFiSecurity/old/10031-fix for sandbox.sql
@@ -47,19 +47,17 @@
  DELETE [dbo].[ClaimSetResourceClaims]  where ResourceClaim_ResourceClaimId in (select ResourceClaimId from ResourceClaims where ResourceName='studentEducationOrganizationResponsibilityAssociation')
  go
   INSERT INTO [dbo].[ClaimSetResourceClaims] ([Action_ActionId] ,[ClaimSet_ClaimSetId] ,[ResourceClaim_ResourceClaimId] , AuthorizationStrategyOverride_AuthorizationStrategyId) 
-   SELECT [ActionId], [ClaimSetId], [ResourceClaimId] , (select AuthorizationStrategyId from AuthorizationStrategies where AuthorizationStrategyName='RelationshipsWithStudentsOnly' and Application_ApplicationId=1)
+   SELECT [ActionId], [ClaimSetId], [ResourceClaimId] , (select AuthorizationStrategyId from AuthorizationStrategies where AuthorizationStrategyName='NoFurtherAuthorizationRequired' and Application_ApplicationId=1)
   FROM [dbo].[ClaimSets] 
   CROSS JOIN [dbo].[Actions]
   CROSS JOIN [dbo].[ResourceClaims] 
-  WHERE [ClaimName] = 'http://ed-fi.org/ods/identity/claims/domains/claimOnly'
+  WHERE [ClaimName] = 'http://ed-fi.org/ods/identity/claims/studentEducationOrganizationResponsibilityAssociation'
   AND [ClaimSetName] IN ('Delaware', 'SIS Vendor', 'Ed-Fi Sandbox','eSchoolPlus')
  
  
 
- --clear out parent permission:
+  --clear out parent permission:
  update ResourceClaims set ParentResourceClaimId =null where ResourceName='studentEducationOrganizationResponsibilityAssociation'
- go
-
 
  -- use EdFi_Delaware_Admin
  -- go
