63ea5ffc2d93b0a5cbf04e751ea486f430831998
Scott Kuykendall <scott.kuykendall@doe.k12.de.us>
2021-05-13 08:10:13 -0400

    Add security for sourcesystemdescriptor

---
+++ Scripts/Delaware Scripts/2022/Security/00020- Security for New Items.sql
@@ -0,0 +1,2 @@
+insert into ResourceClaims (DisplayName, ResourceName, ClaimName, ParentResourceClaimId,Application_ApplicationId)
+values ('sourceSystemDescriptor', 'sourceSystemDescriptor', 'http://ed-fi.org/ods/identity/claims/sourceSystemDescriptor',2,1)
