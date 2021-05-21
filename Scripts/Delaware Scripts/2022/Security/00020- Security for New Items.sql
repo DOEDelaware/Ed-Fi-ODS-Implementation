3d28db3f4c0ef333b19bea3cb2cc4d73d2624b29
Scott Kuykendall <scott.kuykendall@doe.k12.de.us>
2021-05-21 08:15:17 -0400

    fix descriptors fb22688

--- Scripts/Delaware Scripts/2022/Security/00020- Security for New Items.sql
+++ Scripts/Delaware Scripts/2022/Security/00020- Security for New Items.sql
@@ -1,2 +1,6 @@
 insert into ResourceClaims (DisplayName, ResourceName, ClaimName, ParentResourceClaimId,Application_ApplicationId)
 values ('sourceSystemDescriptor', 'sourceSystemDescriptor', 'http://ed-fi.org/ods/identity/claims/sourceSystemDescriptor',2,1)
+
+
+insert into ResourceClaims (DisplayName, ResourceName, ClaimName, ParentResourceClaimId, Application_ApplicationId)
+values ('locationDescriptor','locationDescriptor','http://ed-fi.org/ods/identity/claims/locationDescriptor',2,1)
